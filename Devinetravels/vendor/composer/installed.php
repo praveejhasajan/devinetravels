9265f6201b1c10788ded54ac0df117ab622323f1
Praveen Jha <praveen.jha01@sap.com>
2024-08-21 15:24:44 +0530

    Adding composer lock

---
+++ Devinetravels/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'c5384132/devinetravels',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1d39031101a75ebf8dcb15b3a81395ded592f993',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'c5384132/devinetravels' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1d39031101a75ebf8dcb15b3a81395ded592f993',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
